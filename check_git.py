adafaf060dfc0c924de56c97636a26bf632de752
Aviel Shtern <aviel05484@gmail.com>
2021-10-19 21:21:29 +0300

    implement read_image

--- check_git.py
+++ check_git.py
@@ -1,11 +1,18 @@
 import numpy as np
 from imageio import imread, imwrite
 import matplotlib.pyplot as plt
+from skimage.color import rgb2gray
+
+GRAYSCALE_REPRESENTATION = 1
+RGB_REPRESENTATION = 2
 
 x = np.hstack([np.repeat(np.arange(0, 50, 2), 10)[None, :], np.array([255] * 6)[None, :]])
 grad = np.tile(x, (256, 1))
 
 
-
-
+def read_image(filename, representation):
+    rgb_image = imread(filename).astype(np.float64) / 255
+    return rgb_image if representation == RGB_REPRESENTATION \
+           else rgb2gray(rgb_image) if representation == GRAYSCALE_REPRESENTATION \
+           else -1
 
