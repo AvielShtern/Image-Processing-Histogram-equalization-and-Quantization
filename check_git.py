edbc55fafff04d6b573e8e6effcb18e2fea41334
Aviel Shtern <aviel05484@gmail.com>
2021-10-19 21:03:34 +0300

    add Toy example

--- check_git.py
+++ check_git.py
@@ -1,1 +1,11 @@
-print("hi")
+import numpy as np
+from imageio import imread, imwrite
+import matplotlib.pyplot as plt
+
+x = np.hstack([np.repeat(np.arange(0, 50, 2), 10)[None, :], np.array([255] * 6)[None, :]])
+grad = np.tile(x, (256, 1))
+
+
+
+
+
