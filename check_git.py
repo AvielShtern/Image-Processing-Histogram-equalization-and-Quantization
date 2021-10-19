e310b4975b6310474a29f8f216119a5dc5114813
Aviel Shtern <aviel05484@gmail.com>
2021-10-19 21:57:04 +0300

    implement imdisplay and check whit burger image

--- check_git.py
+++ check_git.py
@@ -1,7 +1,7 @@
 import numpy as np
 from imageio import imread, imwrite
 import matplotlib.pyplot as plt
-from skimage.color import rgb2gray
+from skimage.color import rgb2gray, rgba2rgb
 
 GRAYSCALE_REPRESENTATION = 1
 RGB_REPRESENTATION = 2
@@ -13,6 +13,19 @@ grad = np.tile(x, (256, 1))
 def read_image(filename, representation):
     rgb_image = imread(filename).astype(np.float64) / 255
     return rgb_image if representation == RGB_REPRESENTATION \
-           else rgb2gray(rgb_image) if representation == GRAYSCALE_REPRESENTATION \
-           else -1
+        else rgb2gray(rgb_image) if representation == GRAYSCALE_REPRESENTATION \
+        else -1
 
+
+def imdisplay(filename, representation):
+    im = read_image(filename, representation)
+    print(im.shape)
+    plt.figure()
+    plt.imshow(im, cmap='gray')
+    plt.axis("off")
+    plt.show()
+
+
+if __name__ == '__main__':
+    imdisplay("/Users/avielshtern/Desktop/third_year/IMAGE_PROCESSING/EX/EX1/image2.png", 2)
+    imdisplay("/Users/avielshtern/Desktop/third_year/IMAGE_PROCESSING/EX/EX1/image2.png", 1)
